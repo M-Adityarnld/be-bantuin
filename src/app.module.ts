ced98dd98cd55f4dda768427871386b4f835e1ac
rel1vee <muhzakierbaisyas@gmail.com>
2025-11-01 13:12:03 +0700

    feat: add multiple modules and bcrypt dependency to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,34 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { WalletModule } from './wallet/wallet.module';
+import { ChatModule } from './chat/chat.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { DisputesModule } from './disputes/disputes.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    UsersModule,
+    ServicesModule,
+    OrdersModule,
+    PaymentsModule,
+    WalletModule,
+    ChatModule,
+    ReviewsModule,
+    DisputesModule,
+    NotificationsModule,
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
